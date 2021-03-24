198838698f862cac3e62eb774217fb1f060f6367
Andrew Stone <astone@Andrews-MacBook-Air.local>
2021-03-24 23:04:36 +1100

    “message”

--- src/html.js
+++ src/html.js
@@ -21,16 +21,6 @@ export default function HTML({
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         {headComponents}
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BN7L57VXZT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BN7L57VXZT');
-</script>
-
       </head>
       <body {...bodyAttributes}>
         {preBodyComponents}
